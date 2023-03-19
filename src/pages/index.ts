778e0c2dd2d70a8e1a445d00bada915b69dcf83f
NikolozMazmishvili26 <125471672+NikolozMazmishvili26@users.noreply.github.com>
2023-03-19 14:27:06 +0400

    project general structure
    I installed react on vite/typescript, defined global styles and built the general structure of the project

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home";
+export { default as Detail } from "./Detail";
+export { default as Checkout } from "./Checkout";
+export { default as Category } from "./Category";
